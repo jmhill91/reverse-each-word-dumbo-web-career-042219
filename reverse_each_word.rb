cb2029d2582f98a425a302f54b8b5a7fcdd198fd
John Hill <john.m.hill91@gmail.com>
2019-03-26 19:56:04 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(sent)
   arr=sent.split(" ")
+  newArr=[]
   arr.collect do |word|
-   word.reverse 
-   
-  end
-  
+   new arr += word.reverse 
+   end
+  newArr.join(" ")
 end
